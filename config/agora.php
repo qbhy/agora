e7b6aad43d8c299a00cb67d400af4bceb6437a4e
qbhy <572490755@qq.com>
2019-01-22 21:58:16 +0800

    fixed

---
+++ config/agora.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: qbhy
+ * Date: 2019-01-22
+ * Time: 21:56
+ */
+
+return [
+    'debug' => true,
+];
---
+++ src/Agora.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: qbhy
+ * Date: 2019-01-22
+ * Time: 21:55
+ */
+
+namespace Qbhy\Agora;
+
+use Hanson\Foundation\Foundation;
+
+class Agora extends Foundation
+{
+
+}
